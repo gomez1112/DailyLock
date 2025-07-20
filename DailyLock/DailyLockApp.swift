2fd2c6f8ff8ac7164358c60875fa3deb24e3b584
Gerard Gomez <gerardgomez11@me.comgerardgomez11@me.comgerardgomez11@me.com>
2025-07-20 09:39:37 -0400

    Initial Commit

---
+++ DailyLock/DailyLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyLockApp.swift
+//  DailyLock
+//
+//  Created by Gerard Gomez on 7/20/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DailyLockTests/DailyLockTests.swift
@@ -0,0 +1,16 @@
+//
+//  DailyLockTests.swift
+//  DailyLockTests
+//
+//  Created by Gerard Gomez on 7/20/25.
+//
+
+import Testing
+
+struct DailyLockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
